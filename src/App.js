1eeb49133f486f1bb5dec24f453f00b99e360a25
alinachudaeva <ac@axiros.com>
2020-09-03 11:23:56 +0300

    new  application

--- src/App.js
+++ src/App.js
@@ -1,46 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { count, length } from  './number';
 
-const day = 2;
-const month = 'september';
-const flag = true;
+const element = (<div>Hello my app</div>);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-	      <h1 style={{
-          color: "red",
-          fontSize: 10 + 12
-          }}>
-            First comment {day} {month}
-        </h1>
-        <div>{flag && 'I do it'}</div>
-        <div>
-          {flag ? 'congratulations' : 'do not worry'}
-          {undefined}
-          {false}
-          {true}
-          {null}
-        </div>
-        <div>{count * length}</div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export {element};
