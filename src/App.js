bb4c4ea360e6cdb599ea38cbb3f2274731259015
alinachudaeva <ac@axiros.com>
2020-09-03 11:55:38 +0300

    first composition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
-const element = (<div>Hello my app</div>);
+const ItemList = () => (
+  <div>
+    <ul>
+      <li>a</li>
+      <li>b</li>
+      <li>c</li>
+    </ul>
+  </div>
+);
+const App = () => (
+  <div>
+    <p>first effort to add item list</p>
+    <ItemList />
+  </div>
+);
 
-export {element};
+export default App;
