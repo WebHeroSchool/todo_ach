c7a109a6c67e1f9a01dd996b523379483e815b8d
alinachudaeva <ac@axiros.com>
2020-09-17 12:47:03 +0300

    подключение material-ui урок 25

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import classnames from 'classnames';
 
-const InputItem = () => (<div>InputItem</div>);
+import TextField from '@material-ui/core/TextField';
+
+import  styles from '../../App.module.css';
+
+const InputItem = () => (<div className= {
+    classnames({
+        [styles.input]: true,
+  })
+}>
+    <TextField
+        id="standard-dense"
+        label="Добавить задачу"
+        margin="dense"
+    />
+</div>);
 
 export { InputItem };
 
