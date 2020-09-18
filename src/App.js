5067bbce97dea000bd4967c00eb448ee29194f0b
alinachudaeva <ac@axiros.com>
2020-09-18 19:20:00 +0300

    разбор обработки событий урок 29

--- src/App.js
+++ src/App.js
@@ -9,25 +9,33 @@ class App extends React.Component {
   state = {
     Itemslist: [
       {
-        value: 'первое',
+        value: 'первое - проснуться пораньше',
         isDone: false
       },
       {
-        value:'второе',
+        value:'второе - почитать',
         isDone: true
       },
       {
-        value: 'третье',
+        value: 'третье - погулять',
         isDone: false
       }]
   };
+
+  //constructor(props) {
+  //  super(props)
+  //  this.onButton = this.onButton.bind(this);
+ // }
+
+  onButton = (event) => console.log("press=",event)
+  
 
   render() {
     return (
     <div className={styles.container}>
       <h1 className={styles.container_title}>Важные дела</h1>
       <InputItem />
-      <ItemList items={this.state.Itemslist}/>
+      <ItemList items={this.state.Itemslist} onButton={this.onButton}/>
       <Footer classes={styles} count={3}/>
     </div>
   );
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,18 +2,24 @@ import React from 'react';
 import classnames from 'classnames';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Checkbox from '@material-ui/core/Checkbox';
 
 import  styles from '../../App.module.css';
 
 class Item extends React.Component {
     render() {
         return <span>
+            <Checkbox
+                checked={this.props.isDone}
+                onClick={()=>this.props.onButton(this.props.isDone)}
+                ></Checkbox>
                     <span className={
                         classnames({
                             [styles.item]: true,
                             [styles.done]: this.props.isDone
                         })
-                        }>{this.props.action}
+                        }
+                        >{this.props.action}
                     </span>
                     <span>
                         <IconButton aria-label="delete" >
