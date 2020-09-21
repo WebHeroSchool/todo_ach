2b32486b58eef30e8142e9abe4b80bada56907ed
alinachudaeva <ac@axiros.com>
2020-09-21 15:32:09 +0300

    добавление элемента. урок 32

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -4,7 +4,7 @@ import { Item } from '../Item/Item';
 import styles from '../../App.module.css';
 
 const ItemList = ({ items, onButton, onDelete }) => (<ul>
-    {items.map((item,index) => <li key={item.value}
+    {items.map((item,index) => <li key={item.id}
     >
        <span className={styles.container_list}>
          <Item 
