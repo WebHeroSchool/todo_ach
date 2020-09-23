404b40bf1093eae7c6eacdaab42c8d66e67b5018
alinachudaeva <ac@axiros.com>
2020-09-23 12:32:07 +0300

    не работает

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import CardContent from '@material-ui/core/CardContent';
+import { Octokit } from '@octokit/rest';
+
+const octokit = new Octokit();
 
 const About = () => {
-    const information = "About"
+    const initData = {
+        isLoading: true,
+        reposList: []
+    };
+    const [isLoading, setIsLoading] = useState(initData.isLoading);
+    const [reposList, setReposList] = useState(initData.reposList);
+      
+    useEffect(()=>{
+            octokit.repos.listForUser({
+                username: 'ALCHIK'
+            }).then(data=>{
+                let wrapped_data = data.data;
+                setIsLoading(false);
+                setReposList(wrapped_data);
+            })
+            console.log(reposList)
+    },[isLoading,reposList]) // пробовала оставлять пустым
+
+    useEffect(() => { 
+        console.log('update')
+      });
+
+
+    useEffect(() => {
+        return () => {
+          console.log('will unmount');
+        }
+      }, []);
+
+      
     return (
-        <h1>{information}</h1>
+        <CardContent> <h1>{isLoading ? <CircularProgress /> : 'Обо мне'}</h1>
+        
+        </CardContent>
     );
 
 }
